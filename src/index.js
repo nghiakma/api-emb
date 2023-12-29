4212785650a4fe86620ee1c35b7b01dbf6378acc
nghiakma <lam47788@gmail.com>
2023-12-29 14:27:23 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,75 @@
+const cors = require('cors');
+const express = require('express')
+const app = express()
+const port = 42600
+const hostname = 'localhost'
+const http = require('http');
+const WebSocket = require('ws');
+const fs = require('fs');
+/*
+Cấu hình CORS policy
+ */
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(cors(
+    {
+        origin: [
+            "http://localhost:3000",
+            "http://127.0.0.1:3000",
+            "http://localhost:3001",
+            "http://127.0.0.1:3001",
+        ],
+        // origin: '*',
+        credentials: true,
+        exposedHeaders: ["set-cookie"],
+    }
+));
+
+
+/*
+Cấu hình body parser
+*/
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/*
+Các routers 
+*/
+require('./routers/history.router')(app);
+require('./routers/user.router')(app);
+require('./routers/admin.router')(app);
+
+app.get('/', (req, res) => {
+    // Test Absolute import mapOrde
+    res.end('<h1>Hello World!</h1><hr>')
+  })
+const clients = [];
+
+function broadcast(socket, data) {
+    console.log(clients.length);
+    for (let i = 0; i < clients.length; i++) {
+        if (clients[i] !== socket) {
+            clients[i].send(data.toString());
+        }
+    }
+}
+
+wss.on('connection', (socket, req) => {
+    clients.push(socket);
+    socket.on('message', (message) => {
+        console.log('received: %s', message);
+        broadcast(socket, message);
+    });
+    socket.on('close', () => {
+        const index = clients.indexOf(socket);
+        if (index !== -1) {
+            clients.splice(index, 1);
+        }
+        console.log('disconnected');
+    });
+});
+server.listen(port,hostname, () => {
+    console.log('Server listening on port 42600');
+});
